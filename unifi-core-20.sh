a42c3048dc83066fa4eeffca40d113af4b17cf37
Robson Vaamonde <robsonvaamonde@gmail.com>
2021-09-13 10:37:03 -0300

    Atualizacao

--- unifi-core-20.sh
+++ unifi-core-20.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # ===================== EM DESENVOLVIMENTO - AINDA NÃO ESTÁ FUNCIONANDO PARA PRODUÇÃO =====================
+# =================== NÃO RECOMENDO PARA PRODUÇÃO, VERSÃO DISPONÍVEL NO SNAPCRAFT 5.13.X ==================
 # Autor: Robson Vaamonde
 # Site: www.procedimentosemti.com.br
 # Facebook: facebook.com/ProcedimentosEmTI
@@ -9,7 +10,7 @@
 # Data de atualização: 12/09/2021
 # Versão: 0.02
 # Testado e homologado para a versão do GNU/Linux Ubuntu Server Core 20.x LTS ARM64
-# Testado e homologado para a versão do Unifi Controller 6.2.x, MongoDB 3.6.x, OpenJDK e OpenJRE 8.x
+# Testado e homologado para a versão do Unifi Controller 5.13.x
 #
 # O software UniFi Controller que vem com o sistema Ubiquiti UniFi tem uma interface baseada em Web que facilita
 # a administração, configuração e gerenciamento dos dispositivos Unifi (Access Point AP, Switch, Router, USG, etc).
@@ -64,131 +65,21 @@
 #
 # Vídeo de instalação do GNU/Linux Ubuntu Server Core 20.x LTS: 
 #
-# Variável da Data Inicial para calcular o tempo de execução do script (VARIÁVEL MELHORADA)
-# opção do comando date: +%T (Time)
-HORAINICIAL=$(date +%T)
+# Após a instalação do Unifi Controller acessar a URL: https:IP_UBUNTU_CORE:8443/"
+# Para finalizar a instalação via Web você precisa de uma conta (ID-SSO) no https://account.ui.com"
+# A comunidade do Unifi recomenda utilizar o Navegador Google Chrome para sua configuração
 #
-# Variáveis para validar o ambiente, verificando se o usuário é "root", versão do ubuntu e kernel
-# opções do comando: lsb_release: -r (release), -s (short), 
-# opção do shell script: acento crase ` ` = Executa comandos numa subshell, retornando o resultado
-UBUNTU=$(lsb_release -rs)
-ARQUITETURA=$(uname -m)
+# Atualizando as opções de software do Snap
+sudo snap refresh
 #
-# Variável do caminho do Log dos Script utilizado nesse curso (VARIÁVEL MELHORADA)
-# opções do comando cut: -d (delimiter), -f (fields)
-# $0 (variável de ambiente do nome do comando)
-LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
+# Localizando no Snap a versão do Unifi Controller
+sudo snap find unifi
 #
-# Declarando as variáveis de download do Unifi (Links atualizados no dia 17/01/2021)
-UNIFI="unifi --edge"
-SYSTEM="/var/lib/unifi/system.properties"
+# Instalando o Snap Core
+sudo snap install core
 #
-# Verificando se a Distribuição é >= Core 20 e se a Arquitetura é ARM64 <IF MELHORADO)
-# [ ] = teste de expressão, && = operador lógico AND, == comparação de string, exit 1 = A maioria dos erros comuns na execução
-clear
-if [ "$UBUNTU" == "20" ] && [ "$ARQUITETURA" == "arm64" ]
-	then
-		echo -e "Distribuição é >= Core 20 e a arquitetura é ARM64, continuando com o script..."
-		sleep 5
-	else
-		echo -e "Distribuição não é >= Core 20 ($UBUNTU) e a arquitetura não é ARM64 ($ARQUITETURA)"
-		echo -e "Verifique a distrição e execute novamente o script para validar o ambiente."
-		exit 1
-fi
-#	
-# Verificando se as portas 27017, 8080 e 8443 não estão sendo utilizadas no servidor
-# [ ] = teste de expressão, == comparação de string, exit 1 = A maioria dos erros comuns na execução,
-# $? código de retorno do último comando executado, ; execução de comando, opção do comando nc: -v (verbose)
-# -z (DCCP mode)
-clear
-if [ "$(nc -vz 127.0.0.1 8080 ; echo $?)" == "0" ]
-	then
-		echo -e "A porta: 8080 já está sendo utilizada nesse servidor.\n"
-		echo -e "Verifique a porta e o serviço associada a ela e execute novamente esse script.\n"
-		exit 1
-	else
-		echo -e "A porta: 8080 está disponível, continuando com o script..."
-        sleep 5
-fi
-#
-if [ "$(nc -vz 127.0.0.1 8443 ; echo $?)" == "0" ]
-	then
-		echo -e "A porta: 8443 já está sendo utilizada nesse servidor.\n"
-		echo -e "Verifique a porta e o serviço associada a ela e execute novamente esse script.\n"
-		exit 1
-	else
-		echo -e "A porta: 8443 está disponível, continuando com o script..."
-        sleep 5
-fi
-#
-if [ "$(nc -vz 127.0.0.1 27017 ; echo $?)" == "0" ]
-	then
-		echo -e "A porta: 27017 já está sendo utilizada nesse servidor.\n"
-		echo -e "Verifique a porta e o serviço associada a ela e execute novamente esse script.\n"
-		exit 1
-	else
-		echo -e "A porta: 27017 está disponível, continuando com o script..."
-        sleep 5
-fi
-#
-# Script de instalação do Unifi Controller no GNU/Linux Ubuntu Core 20
-# opção do comando echo: -e (enable interpretation of backslash escapes), \n (new line)
-# opção do comando hostname: -I (all IP address)
-# opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
-# opção do comando cut: -d (delimiter), -f (fields)
-echo -e "Início do script $0 em: $(date +%d/%m/%Y-"("%H:%M")")\n" &>> $LOG
-clear
-#
-echo
-echo -e "Instalação do Unifi Controller no GNU/Linux Ubuntu Core Server 20.x\n"
-echo -e "Após a instalação do Unifi Controller acessar a URL: https://$(hostname -I | cut -d' ' -f1):8443/\n"
-echo -e "Para finalizar a instalação via Web você precisa de uma conta (ID-SSO) no https://account.ui.com\n"
-echo -e "A comunidade do Unifi recomenda utilizar o Navegador Google Chrome para sua configuração\n"
-echo -e "Aguarde, esse processo demora um pouco dependendo do seu Link de Internet...\n"
-sleep 5
-#
-echo -e "Atualizando as opções de software do Snap, aguarde..."
-	#opção do comando: &>> (redirecionar a saída padrão)
-	sudo snap refresh &>> $LOG
-echo -e "Opções de software atualizadas com sucesso!!!, continuando com o script...\n"
-sleep 5
-#
-echo -e "Localizando o Snap do Unifi Controller, aguarde..."
-	# opção do comando: &>> (redirecionar a saída padrão)
-	sudo snap find unifi | grep UniFi &>> $LOG
-echo -e "Sistema atualizado com sucesso!!!, continuando com o script...\n"
-sleep 5
-#
-echo -e "Instalando o Unifi Controller, aguarde...\n"
-#
-echo -e "Instalando o Unifi Controller, aguarde..."
-	# opção do comando: &>> (redirecionar a saída padrão)
-	sudo snap install core &>> $LOG
-	sudo snap install $UNIFI &>> $LOG
-echo -e "Unifi Controller instalado com sucesso!!!, continuando com o script...\n"
-sleep 5
-#
-echo -e "Verificando as portas de conexões do MongoDB e do Unifi Controller, aguarde..."
-	# opção do comando netstat: -a (all), -n (numeric)
-	# opção do comando grep: \| (função OU)
-	ss -tua | grep '27017\|8080\|8443'
-echo -e "Portas de conexões verificadas com sucesso!!!, continuando com o script..."
-sleep 5
-echo
-#
-echo -e "Instalação do Unifi Controller feita com Sucesso!!!."
-	# script para calcular o tempo gasto (SCRIPT MELHORADO, CORRIGIDO FALHA DE HORA:MINUTO:SEGUNDOS)
-	# opção do comando date: +%T (Time)
-	HORAFINAL=$(date +%T)
-	# opção do comando date: -u (utc), -d (date), +%s (second since 1970)
-	HORAINICIAL01=$(date -u -d "$HORAINICIAL" +"%s")
-	HORAFINAL01=$(date -u -d "$HORAFINAL" +"%s")
-	# opção do comando date: -u (utc), -d (date), 0 (string command), sec (force second), +%H (hour), %M (minute), %S (second), 
-	TEMPO=$(date -u -d "0 $HORAFINAL01 sec - $HORAINICIAL01 sec" +"%H:%M:%S")
-	# $0 (variável de ambiente do nome do comando)
-	echo -e "Tempo gasto para execução do script $0: $TEMPO"
-echo -e "Pressione <Enter> para concluir o processo."
-# opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
-echo -e "Fim do script $0 em: $(date +%d/%m/%Y-"("%H:%M")")\n" &>> $LOG
-read
-exit 1
+# Instalando o Unifi Controller
+sudo snap install unifi --edge
+
+# Verificando as portas do Unifi Controller
+ss -tua | grep '27017\|8080\|8443'
